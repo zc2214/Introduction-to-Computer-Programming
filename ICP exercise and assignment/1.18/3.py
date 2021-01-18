5b519fe6f1a770404aa24aaa7181b96fd9b414e0
James Chen <71540992+zc2214@users.noreply.github.com>
2021-01-18 17:30:20 +0800

    Update 3.py

--- ICP exercise and assignment/1.18/3.py
+++ ICP exercise and assignment/1.18/3.py
@@ -1,7 +1,5 @@
 import random
-old_number = 0
+maxnum = 0
 for i in range(0,100):
-    new_number = random.randint(1,100)
-    if new_number > old_number:
-        old_number = new_number
-print(old_number)
+    maxnum=max(maxnum, random.randint(1,100))
+print(maxnum)
